2cc4f0c1f00d16e0dd90b86ba9fa149c98f59427
Miguel Terán <miguel.teranj02@gmail.com>
2025-12-16 08:19:58 -0400

    feat(config, layout): integrate Astro Icon, update TS paths, refactor index page
    - Add Astro Icon support and necessary dev dependencies
    - Update TypeScript paths for cleaner imports
    - Refactor index.astro to use layout and section components for better structure and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import icon from "astro-icon";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: { enabled: false },
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  integrations: [
+    icon({
+      include: {
+        ic: [
+          "twotone-rocket-launch",
+          "twotone-storefront",
+          "baseline-whatsapp",
+        ],
+        "icon-park-twotone": ["trend", "check-one"],
+        "ant-design": ["thunderbolt-twotone"],
+        mdi: ["register"],
+        bxs: ["up-arrow"],
+      },
+    }),
+  ],
 });
